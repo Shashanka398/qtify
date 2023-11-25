9eb2f285bc748659b683741b313aa31d3f4073aa
shashank398@gmail.com <you@example.com>
2023-11-25 21:10:44 +0530

    qtify

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import navBar from './components/navBar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <navBar/>
+    </div>
+  );
+}
+
+export default App;
